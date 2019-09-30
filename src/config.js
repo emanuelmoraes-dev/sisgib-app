5e9fcd9a92f2b43c460ac46b7ecf8a0ed85f6f7e
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-09-29 22:42:22 -0300

    url de produção atualizada

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ if (process.env.NODE_ENV === 'development') {
 	exports.urlBase = 'http://localhost:3000'
 	exports.origin = 'http://localhost:8080'
 } else {
-	exports.urlBase = 'http://localhost'
-	exports.origin = 'http://localhost'
+	exports.urlBase = 'https://sisgib.herokuapp.com'
+	exports.origin = 'https://sisgib.herokuapp.com'
 }
