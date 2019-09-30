ec7d550e2fe417bb3dd58382119c8f218183893c
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-09-30 15:56:50 -0300

    urlBase de produção resolvida

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ if (process.env.NODE_ENV === 'development') {
 	exports.urlBase = 'http://localhost:3000'
 	exports.origin = 'http://localhost:8080'
 } else {
-	exports.urlBase = 'sisgib.herokuapp.com'
+	exports.urlBase = '/'
 	exports.origin = 'sisgib.herokuapp.com'
 }
