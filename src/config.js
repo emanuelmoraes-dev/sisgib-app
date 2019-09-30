fbacea3fe0af367a0ec347cc8f56bec98c07ea39
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-09-30 16:27:07 -0300

    urlBase consertado

--- src/config.js
+++ src/config.js
@@ -2,6 +2,9 @@ if (process.env.NODE_ENV === 'development') {
 	exports.urlBase = 'http://localhost:3000'
 	exports.origin = 'http://localhost:8080'
 } else {
-	exports.urlBase = '/'
+	if (location.protocol === 'https:')
+		exports.urlBase = 'https://sisgib.herokuapp.com'
+	else
+		exports.urlBase = 'http://sisgib.herokuapp.com'
 	exports.origin = 'sisgib.herokuapp.com'
 }
